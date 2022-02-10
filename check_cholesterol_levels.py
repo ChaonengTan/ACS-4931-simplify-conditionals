978486145b3ceec6e9ed0036ce8448388965575f
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-02-10 13:36:01 -0800

    challenge 17

--- check_cholesterol_levels.py
+++ check_cholesterol_levels.py
@@ -7,15 +7,19 @@ total_cholostrol = 70
 ldl = 30
 triglyceride = 120
 
-if total_cholostrol < 200 and ldl < 100 and triglyceride < 150:
+goodCholestrol = total_cholostrol < 200 and ldl < 100 and triglyceride < 150
+highCholestrol = 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200
+borderlineCholestrol = 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200
+
+if goodCholestrol:
     # good level
     print('*** Good level of cholestrol ***')
-elif 200 < total_cholostrol > 240 or ldl > 160 or triglyceride >= 200:
+elif highCholestrol:
     # High cholestrol level
     print('*** High cholestrol level ***')
     print('start taking pills such as statins')
     print('start TLC diet')
-elif 200 <total_cholostrol < 240 or 130 < ldl < 160 or 150 <= triglyceride < 200:
+elif borderlineCholestrol:
     #TLC_diet
     print('*** Borderline to moderately elevated ***')
     print("Start TLC diet")
